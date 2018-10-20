4c5d50b5ea8ca53d7fff55e6fe600aa555a8f840
omar jandali <omarjandali@omars-MBP.attlocal.net>
2018-10-19 22:18:16 -0700

    setup the server in the index.js file

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const parser = require("body-parser");
+const path = require("path");
+const morgan = require("morgan");
+const router = require("./router")
+
+const server = express()
+const listeningPort = 4444;
+
+server.use(morgan("dev"));
+server.use(parser.json())
+server.use(parser.urlencoded({extended:true}))
+server.use(express.static(path.join(__dirname, "../client/src")))
+
+server.use("/api", router)
+
+server.listen(listeningPort, () => {console.log("server connected")})
